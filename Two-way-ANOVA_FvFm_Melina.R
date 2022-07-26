9bd0142c7ac5624463edc5c1b60826f246c3f7a9
melinargb13 <51415047+melinargb13@users.noreply.github.com>
2022-07-26 02:38:08 +0200

    Add files via upload
    FvFm_ED50s

---
+++ Two-way-ANOVA_FvFm_Melina.R
@@ -0,0 +1,35 @@
+setwd("D:/Desktop/UoK/Tesis/r_thesis/FvFm_ED50s")
+
+library(lmerTest)
+library(emmeans)
+library(sjPlot)
+library(drc)
+library(ggplot2)
+library(Rmisc)
+
+#### Load and view file
+input<- read.delim("PAM_Ed_StatsAnalysis.txt", sep= '\t')
+View(input)
+str(input)
+
+data_T1<-subset(input, Timepoint== "T1")
+data_T2<-subset(input, Timepoint== "T2")
+
+Fv_FmANOVA1<-aov(PAM~Temperature*Treatment, data=data_T1) 
+anova(Fv_FmANOVA1)
+
+Fv_FmANOVA2<-aov(PAM~Temperature*Treatment, data=data_T2) 
+anova(Fv_FmANOVA2)
+
+#Temperature is a significant factor
+#Treatment (Arduino vs. InkBird) is not a significant factor 
+
+#Another way to do it#
+
+twoANOVA <-aov(PAM~Treatment * factor(Temperature), data=input)
+summary(twoANOVA)
+
+################
+##T-test ED50s##
+################
+
